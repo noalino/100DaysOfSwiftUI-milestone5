0c5f122727b3ee75ea65f544603e337897d907cc
Noalino <bgelineau.dev@gmail.com>
2024-02-23 14:57:31 +0100

    Add photo picker

--- RememberNames/ContentView.swift
+++ RememberNames/ContentView.swift
@@ -5,17 +5,58 @@
 //  Created by Noalino on 23/02/2024.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
+import StoreKit
 import SwiftUI
 
+struct ImageWithName: Identifiable {
+    let id = UUID()
+    let name: String
+    let image: Image
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var items: [ImageWithName] = []
+    @State private var selectedItem: PhotosPickerItem?
+
+    @ViewBuilder
+    var ListView: some View {
+        if items.isEmpty {
+            ContentUnavailableView("No names", systemImage: "photo.badge.plus", description: Text("Tap \"Add a name\" to import a name"))
+        } else {
+            List(items) { item in
+                HStack {
+                    item.image
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .clipShape(.circle)
+
+                    Spacer()
+
+                    Text(item.name)
+                }
+            }
         }
-        .padding()
+    }
+
+    var body: some View {
+        NavigationStack {
+            ListView
+            .toolbar {
+                PhotosPicker("Add a name", selection: $selectedItem)
+                    .onChange(of: selectedItem, addImage)
+            }
+            .navigationTitle("RememberNames")
+        }
+    }
+
+    func addImage() {
+        Task {
+            guard let image = try await selectedItem?.loadTransferable(type: Image.self) else { return }
+            items.append(ImageWithName(name: "test", image: image))
+        }
     }
 }
 
