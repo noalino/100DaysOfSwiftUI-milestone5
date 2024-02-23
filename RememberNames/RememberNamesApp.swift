a6cfbe78232c6282b20f8580d2b2b17cb5b9cef1
Noalino <bgelineau.dev@gmail.com>
2024-02-23 14:47:31 +0100

    Initial Commit

---
+++ RememberNames/RememberNamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberNamesApp.swift
+//  RememberNames
+//
+//  Created by Noalino on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RememberNamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
